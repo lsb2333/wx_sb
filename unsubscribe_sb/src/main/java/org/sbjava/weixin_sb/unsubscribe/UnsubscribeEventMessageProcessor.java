c47eb0d2823808ba38d1987ebcda5f1e46ac4bf9
xiao <xiao@xiao-PC>
2019-05-11 09:42:31 +0800

    更新

--- unsubscribe_sb/src/main/java/org/sbjava/weixin_sb/unsubscribe/UnsubscribeEventMessageProcessor.java
+++ unsubscribe_sb/src/main/java/org/sbjava/weixin_sb/unsubscribe/UnsubscribeEventMessageProcessor.java
@@ -8,12 +8,15 @@ import org.sbjava.commons.domain.User;
 import org.sbjava.commons.domain.event.EventInMessage;
 import org.sbjava.commons.processors.EventMessageProcessor;
 import org.sbjava.commons.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("unsubscribeMessageProcessor")
 public class UnsubscribeEventMessageProcessor implements EventMessageProcessor {
 
+	private static Logger LOG = LoggerFactory.getLogger(UnsubscribeEventMessageProcessor.class);
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -25,6 +28,8 @@ public class UnsubscribeEventMessageProcessor implements EventMessageProcessor {
 			// 非取消关注事件，不处理
 			return;
 		}
+		
+		LOG.trace("处理取消关注的消息："+msg);
 		
 		//由于方法上面有@Transactional注解，调用对象的set方法，会自动更新到数据库
 		User user = this.userRepository.findByOpenId(msg.getFromUserName());
